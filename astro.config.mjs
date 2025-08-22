e9a9451f0667eda93381c4de49d183c2f9b06534
apritykin <apritykin@gmail.com>
2025-08-22 09:57:46 -0400

    added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const isProd = process.env.NODE_ENV === 'production';
+const site = isProd
+  ? "https://apritykin.github.io"
+  : "http://localhost:4321";
+
 export default defineConfig({
-  site: "https://apritykin.github.io",
+  site,
   integrations: [tailwind(), icon(), mdx(), sitemap()]
 });
